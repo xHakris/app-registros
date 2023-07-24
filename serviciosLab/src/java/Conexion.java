ee465e4e72c0f10035ff2c10b6bbb41fb9d907ea
YomalD12 <yomald1212@gmail.com>
2023-07-23 21:04:07 -0500

    creacion de conexion con la bd

---
+++ serviciosLab/src/java/Conexion.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/registrolaboratorios";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    // Agrega el siguiente método para cerrar la conexión si es necesario
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Conexión cerrada exitosamente.");
+            } catch (SQLException e) {
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+}
