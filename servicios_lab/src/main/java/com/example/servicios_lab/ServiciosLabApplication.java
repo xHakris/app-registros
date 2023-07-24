ce691ade552284fc358082ba88834b81e5f3225f
YomalD12 <yomald1212@gmail.com>
2023-07-24 15:38:38 -0500

    servicios en spring boot

---
+++ servicios_lab/src/main/java/com/example/servicios_lab/ServiciosLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicios_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosLabApplication.class, args);
+	}
+
+}
